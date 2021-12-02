442cb229815fa62c04284a90f2d9a6e3d2068f91
F-19-F <58457605+F-19-F@users.noreply.github.com>
2021-12-02 12:05:33 +0800

    add support for linux and add new methods

--- ec.py
+++ ec.py
@@ -1,11 +1,26 @@
-import ctypes
-lib = ctypes.WinDLL(".\inpoutx64.dll")
-inb = lib.DlPortReadPortUchar
-outb = lib.DlPortWritePortUchar
+import platform
 EC_IBF_BIT = 0b10
 EC_OBF_BIT = 0b01
 EC_CMD_STATUS_REGISTER_PORT = 0x66
 EC_DATA_REGISTER_PORT = 0x62
+if (platform.system() =="Windows"):
+    import ctypes
+    lib = ctypes.WinDLL(".\inpoutx64.dll")
+    inb = lib.DlPortReadPortUchar
+    outb = lib.DlPortWritePortUchar
+elif (platform.system() =="Linux"):
+    # pip3 install portio
+    import portio
+    def linux_inb(port):
+        return portio.inb(port)
+    inb = linux_inb
+    def linux_outb(port,data):
+        return portio.outb(data,port)
+    outb = linux_outb
+    res_sc = portio.ioperm(EC_CMD_STATUS_REGISTER_PORT,1,1)
+    res_data = portio.ioperm(EC_DATA_REGISTER_PORT,1,1)
+    if (res_sc != 0 or res_data !=0):
+        raise Exception("ioperm error")
 
 
 class EC:
