b0a656e25fc0960635a861e5241f52edae538075
F-19-F <58457605+F-19-F@users.noreply.github.com>
2021-12-02 21:49:25 +0800

    add new methods

--- officialway.py
+++ officialway.py
@@ -67,6 +67,41 @@ class Clevo_EC():
             "duty": int(100*duty/255+0.5)
         }
 
+    @staticmethod
+    def GetFan12RPM():
+        EC.Write(Clevo_EC.FDAT, 0x03)
+        EC.Write(Clevo_EC.FCMD, 0xC0)
+        data1 = EC.Read(Clevo_EC.FBUF)
+        data2 = EC.Read(Clevo_EC.FBUF1)
+        fan1rpm = (data1 << 0x08) | data2
+        EC.Write(Clevo_EC.FCMD, 0x00)
+        EC.Write(Clevo_EC.FDAT, 0x04)
+        EC.Write(Clevo_EC.FCMD, 0xC0)
+        data3 = EC.Read(Clevo_EC.FDAT)
+        data4 = EC.Read(Clevo_EC.FBUF)
+        fan2rpm = (data3 << 0x08) | data4
+        EC.Write(Clevo_EC.FCMD, 0x00)
+        if fan1rpm != 0:
+            fan1rpm = int(2156220/fan1rpm + 0.5)
+        if fan2rpm != 0:
+            fan2rpm = int(2156220/fan2rpm + 0.5)
+        return {
+            'fan1rpm': fan1rpm,
+            'fan2rpm': fan2rpm
+        }
+
+    @staticmethod
+    def GetPowerStateForCamera():
+        EC.Write(Clevo_EC.FDAT, 0xA1)
+        EC.Write(Clevo_EC.FCMD, 0xB8)
+        stat = EC.Read(Clevo_EC.FDAT)
+        EC.Write(Clevo_EC.FCMD, 0x00)
+        if (stat & 0b01 == 1):
+            # True to enable
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
     Clevo_EC.SetFanDuty(1, 100)
@@ -74,6 +109,11 @@ if __name__ == '__main__':
     Clevo_EC.SetWhiteLedKB(5)
     print("FAN1:", Clevo_EC.Fan1Info())
     print("FAN2:", Clevo_EC.Fan2Info())
+    print(Clevo_EC.GetFan12RPM())
+    if Clevo_EC.GetPowerStateForCamera():
+        print("Camera on")
+    else:
+        print("camera off")
     time.sleep(1)
     print("FAN1:", Clevo_EC.Fan1Info())
     print("FAN2:", Clevo_EC.Fan2Info())
